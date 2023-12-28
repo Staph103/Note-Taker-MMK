abd21436ccde9419bc08e4013c57326f81f5b5ba
MMK <staph103@gmail.com>
2023-12-28 15:10:07 -0500

    Add some get calls and a listner

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,26 @@
+//Variable decleration and require section 
+
 const PORT = 3003;
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Working on port ${PORT}!`);
+});
