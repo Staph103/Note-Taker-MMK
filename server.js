c4e84ec3dfdaa0e5e589cb68a49d970567bd00c7
MMK <staph103@gmail.com>
2024-01-02 15:04:35 -0500

    Fixed delete functionality

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Variable decleration and require section 
 
-const PORT = 3009;
-const { randomUUID } = require('crypto');
+const PORT = process.env.PORT || 3009;
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -12,36 +11,76 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('develop/public'));
+app.use(express.static('public'));
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 })
-app.get('/api/notes',(req,res)=>{
-  fs.readFile('develop/db/db.json','utf8',(err, data) =>{
-    if(err){
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
       console.error(err);
       return res.status(500).send('Error reading notes file');
     }
     res.json(JSON.parse(data));
   })
 })
-app.post('/api/notes',(req,res)=>{
-  const newNote = {...req.body,id: randomUUID}
-  fs.readFile('develop/db/db.json','utf8',(err, data) =>{
-    if(err){
+app.post('/api/notes', (req, res) => {
+  const randomID = Math.floor((1 + Math.random()) * 100)
+    
+  const newNote = { ...req.body, id: randomID }
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
       console.error(err);
       return res.status(500).send('Error reading notes file');
     }
-  
-})
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    console.log(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error reading notes file');
+      }
+      res.json(newNote);
+    })
+  })
+});
 
+app.delete('/api/notes/:id', (req, res) => {
+  console.log("hello", req.params.id);
+  const deleteNotes = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading notes file');
+    }
+    let notes = JSON.parse(data);
+    // for (let index = 0; index < notes.length; index++) {
+    //   const removeNotes = notes[index];
+    //   if (removeNotes.id == deleteNotes) {
+    //     notes = notes.splice(index, 1);
+    //     console.log(notes);
+    //   }
+      let filteredNotes = notes.filter((note)=> {
+        console.log(note.id, deleteNotes);
+       return note.id != deleteNotes})
+  console.log(filteredNotes);
+
+    fs.writeFile('./db/db.json', JSON.stringify(filteredNotes), (err) => {
+      if (!err) {
+        res.json(JSON.stringify(filteredNotes));
+        
+      }
+    })
+  })
+})
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
-console.log('node is working');
